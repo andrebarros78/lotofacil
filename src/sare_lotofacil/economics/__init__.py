8757e0fb3636adfc28f8a93a2d5d40eb1dbe2003
andrebarros78 <andrefitoprime@gmail.com>
2026-09-14 17:27:25 -0300

    science: exportar contrato de horizonte econômico

--- src/sare_lotofacil/economics/__init__.py
+++ src/sare_lotofacil/economics/__init__.py
@@ -1,5 +1,5 @@
 """Cálculos econômicos auditáveis da SARE Operational."""
 
-from .core import EconomicAudit, calculate_economic_audit
+from .core import EconomicAudit, HorizonCashflow, calculate_economic_audit, summarize_horizon_cashflow
 
-__all__ = ["EconomicAudit", "calculate_economic_audit"]
+__all__ = ["EconomicAudit", "HorizonCashflow", "calculate_economic_audit", "summarize_horizon_cashflow"]
